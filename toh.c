0099937c1ee18da0a46664582d8be352d078d620
JeevithaEswaran <jeevithaeswaran1@gmail.com>
2016-07-10 11:27:20 +0530

    Update toh.c

--- toh.c
+++ toh.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include<conio.h>
+
 void towerOfHanoi(int,char,char,char);
 void towerOfHanoi(int n, char frompeg, char topeg, char auxpeg)
 {
     if (n == 1)
     {
-        printf("\n Move disk 1 from rod %c to rod %c", fromrod, torod);
+        printf("\n Move disk 1 from rod %c to rod %c", frompeg, topeg);
         return;
     }
     towerOfHanoi(n-1, frompeg, auxpeg, topeg);
     printf("\n Move disk %d from rod %c to rod %c", n, frompeg, topeg);
     towerOfHanoi(n-1, auxpeg, topeg, frompeg);
-    getch();
+    
 }
  
 int main()
@@ -20,5 +20,3 @@ int main()
     towerOfHanoi(n, 'A', 'C', 'B');  
     return 0;
 }
-
-    
